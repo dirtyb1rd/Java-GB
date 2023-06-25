8658639d668f3b56a18d4360f297c42dcb9da2fd
dirtyb1rd <yeskov777.888@gmail.com>
2023-06-25 19:37:52 +0300

    prefinal task completed

---
+++ Lesson3.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+        ArrayList<String> list1 = new ArrayList<>(Arrays.asList("Earth", "Mars", "Jupiter", "Venus", "Pluton"));
+        int count = 0;
+        for (int i = 0; i < args.length; i++) {
+            
+        }
+    }
+}
